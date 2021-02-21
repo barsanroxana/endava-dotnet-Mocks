38cafe501f6f090da06c29bdfc3cef2a20f3b19b
Roxana <barsanroxana20@yahoo.com>
2021-02-21 18:15:15 +0200

    Tema sesiune4: adaugare project nou FoodPal.Orders.Mock.GreekCruisine.Api si worker pentre PrestoPizza

---
+++ src/FoodPal.Orders.Mock.GreekCuisine.Api/BackgroundProcessing/IBackgroundTaskQueue.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FoodPal.Orders.Mock.GreekCuisine.Api.BackgroundProcessing
+{
+
+	public interface IBackgroundTaskQueue
+	{
+		void QueueBackgroundWorkItem(Func<CancellationToken, Task> workItem);
+
+		Task<Func<CancellationToken, Task>> DequeueAsync(CancellationToken cancellationToken);
+	}
+}
---
+++ src/FoodPal.Orders.Mock.GreekCuisine.Api/Controllers/ApiBaseController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FoodPal.Orders.Mock.GreekCuisine.Api.Controllers
+{
+    [ApiController]
+	[Produces("application/json")]
+	[Route("v{version:apiVersion}/[controller]")]
+	public class ApiBaseController : ControllerBase
+	{
+
+	}
+}
